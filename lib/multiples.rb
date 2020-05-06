d6bc6fc2696327bc25170e0a68750d515ef5a3b7
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-05-06 13:21:57 -0500

    was fun

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    multiples_of_3_below_1000 = []
+    multiples_of_5_below_1000 = []
+    counter = 3;
+    while counter < limit do 
+      multiples_of_3_below_1000 << counter
+      counter += 3 
+    end
+    counter = 5;
+    while counter < limit do 
+      multiples_of_3_below_1000 << counter
+      counter += 5 
+    end
+
+    (multiples_of_3_below_1000 + multiples_of_5_below_1000).uniq.sort 
+end 
+
+def sum_multiples(limit)
+  collect_multiples(limit).sum
+end 
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end 
+
+  def collect_multiples
+    multiples_of_3_below_1000 = []
+    multiples_of_5_below_1000 = []
+    counter = 3;
+    while counter < @limit do 
+      multiples_of_3_below_1000 << counter
+      counter += 3 
+    end
+    counter = 5;
+    while counter < @limit do 
+      multiples_of_3_below_1000 << counter
+      counter += 5 
+    end
+
+    (multiples_of_3_below_1000 + multiples_of_5_below_1000).uniq.sort 
+  end 
+
+  def sum_multiples
+    self.collect_multiples.sum
+  end 
+
+end 
